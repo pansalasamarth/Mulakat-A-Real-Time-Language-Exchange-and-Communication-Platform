4964262c078deb3df5ae4f3e07333eec24ddfe84
Samarth Pansala <pansalasamarth.com>
2026-02-18 17:34:33 +0530

    feat: implement user signup functionality with authentication routes and MongoDB connection

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { signup } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.get("/login", (req, res)=> {
+    res.send("login page");
+});
+router.get("/logout", (req, res)=> {
+    res.send("logout page");
+});
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import "dotenv/config";
 
+import authRoutes from "./routes/auth.route.js";
+
+import { connectDB } from "./lib/db.js";
+
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello World");
@@ -12,4 +18,5 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
