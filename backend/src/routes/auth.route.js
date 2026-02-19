f5a6d4427226c1cb1ab5ce1dcbf91aeef1b439bd
Samarth Pansala <pansalasamarth.com>
2026-02-19 11:59:41 +0530

    feat: add login functionality with JWT authentication and cookie handling

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { login, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
-router.get("/login", (req, res)=> {
-    res.send("login page");
-});
+router.post("/login", login);
 router.get("/logout", (req, res)=> {
     res.send("logout page");
 });
