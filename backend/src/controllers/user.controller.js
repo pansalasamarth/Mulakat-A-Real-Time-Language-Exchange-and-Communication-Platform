d48e9fa64976c3b125dc735c5ae3dec4d94a333d
Samarth Pansala <pansalasamarth.com>
2026-02-23 15:55:17 +0530

    feat: add user recommendation and friend request models

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,21 @@
+import User from "../models/User.js";
+import FriendRequest from "../models/FriendRequest.js";
+
+export async function getRecommendedUsers(req, res) {
+  try {
+    const currentUserId = req.user.id;
+    const currentUser = req.user;
+
+    const recommendedUsers = await User.find({
+      $and: [
+        { _id: { $ne: currentUserId } }, //exclude current user
+        { _id: { $nin: currentUser.friends } }, // exclude current user's friends
+        { isOnboarded: true },
+      ],
+    });
+    res.status(200).json(recommendedUsers);
+  } catch (error) {
+    console.error("Error in getRecommendedUsers controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
