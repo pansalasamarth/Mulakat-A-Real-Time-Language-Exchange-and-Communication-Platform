a6f9187bc1cc474f75203ae24b4559886286d287
Samarth Pansala <pansalasamarth.com>
2026-04-13 14:26:23 +0530

    feat: add error handling for friend profile picture in FriendCard component

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -8,7 +8,10 @@ const FriendCard = ({ friend }) => {
         {/* USER INFO */}
         <div className="flex items-center gap-3 mb-3">
           <div className="avatar size-12">
-            <img src={friend.profilePic} alt={friend.fullName} />
+            <img src={friend.profilePic} alt={friend.fullName} 
+            onError={(e) => {
+              e.target.src = "https://api.dicebear.com/7.x/avataaars/svg?seed=87";
+            }}/>
           </div>
           <h3 className="font-semibold truncate">{friend.fullName}</h3>
         </div>
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -55,7 +55,10 @@ const Sidebar = () => {
         <div className="flex items-center gap-3">
           <div className="avatar">
             <div className="w-10 rounded-full">
-              <img src={authUser?.profilePic} alt="User Avatar" />
+              <img src={authUser?.profilePic} alt="User Avatar" 
+              onError={(e) => {
+                e.target.src = "https://api.dicebear.com/7.x/avataaars/svg?seed=87";
+              }}/>
             </div>
           </div>
           <div className="flex-1">
