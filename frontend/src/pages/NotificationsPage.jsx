57f943fcc069953f0bcb7271cf9bc63cefb4d40d
Samarth Pansala <pansalasamarth.com>
2026-04-16 09:20:44 +0530

    feat: add fallback image for sender profile picture on error in NotificationsPage component

--- frontend/src/pages/NotificationsPage.jsx
+++ frontend/src/pages/NotificationsPage.jsx
@@ -51,7 +51,10 @@ const NotificationsPage = () => {
                         <div className="flex items-center justify-between">
                           <div className="flex items-center gap-3">
                             <div className="avatar w-14 h-14 rounded-full bg-base-300">
-                              <img src={request.sender.profilePic} alt={request.sender.fullName} />
+                              <img src={request.sender.profilePic} alt={request.sender.fullName} 
+                              onError={(e) => {
+                                e.target.src = "https://api.dicebear.com/7.x/avataaars/svg?seed=87";
+                              }}/>
                             </div>
                             <div>
                               <h3 className="font-semibold">{request.sender.fullName}</h3>
